07b35d78183c93ed42eb89d4770c0a114c33ed26
Arun Suresh <arunsureshampadath@gmail.com>
2020-05-09 20:46:23 +0530

    env vars added

--- bin/release.sh
+++ bin/release.sh
@@ -11,22 +11,22 @@ if [[ -z "$kob_version" ]];
  	    exit 0
 fi
 
-if [ -z "$KOB_NAMESPACE" ];
-    then
-        KOB_NAMESPACE=${KOB_NAMESPACE:-hyperledgerkochi}
+# if [ -z "$KOB_NAMESPACE" ];
+#     then
+#         KOB_NAMESPACE=${KOB_NAMESPACE:-hyperledgerkochi}
 
-fi
-echo $KOB_NAMESPACE
-if [ -z "$KOB_ARCHIVE_DOWNLOAD_REPO" ];
-    then
-        KOB_ARCHIVE_DOWNLOAD_REPO=${KOB_ARCHIVE_DOWNLOAD_REPO:-KOBman_target_repo}
-fi
-echo $KOB_ARCHIVE_DOWNLOAD_REPO
-if  [ -z "$KOB_DIR" ];
-    then
-        KOB_DIR=~/KOBman
-fi
-echo $KOB_DIR
+# fi
+# echo $KOB_NAMESPACE
+# if [ -z "$KOB_ARCHIVE_DOWNLOAD_REPO" ];
+#     then
+#         KOB_ARCHIVE_DOWNLOAD_REPO=${KOB_ARCHIVE_DOWNLOAD_REPO:-KOBman_target_repo}
+# fi
+# echo $KOB_ARCHIVE_DOWNLOAD_REPO
+# if  [ -z "$KOB_DIR" ];
+#     then
+#         KOB_DIR=~/KOBman
+# fi
+# echo $KOB_DIR
 
 vars="$kob_version $KOB_ARCHIVE_DOWNLOAD_REPO $KOB_NAMESPACE"
 cd ~/KOBman
