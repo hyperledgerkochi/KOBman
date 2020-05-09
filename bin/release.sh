f36a97dde609410daa96a521c6cf8adacef07596
Arun Suresh <arunsureshampadath@gmail.com>
2020-05-09 20:59:41 +0530

    Update version of Release to test-tag19

--- bin/release.sh
+++ bin/release.sh
@@ -11,23 +11,6 @@ if [[ -z "$kob_version" ]];
  	    exit 0
 fi
 
-# if [ -z "$KOB_NAMESPACE" ];
-#     then
-#         KOB_NAMESPACE=${KOB_NAMESPACE:-hyperledgerkochi}
-
-# fi
-# echo $KOB_NAMESPACE
-# if [ -z "$KOB_ARCHIVE_DOWNLOAD_REPO" ];
-#     then
-#         KOB_ARCHIVE_DOWNLOAD_REPO=${KOB_ARCHIVE_DOWNLOAD_REPO:-KOBman_target_repo}
-# fi
-# echo $KOB_ARCHIVE_DOWNLOAD_REPO
-# if  [ -z "$KOB_DIR" ];
-#     then
-#         KOB_DIR=~/KOBman
-# fi
-# echo $KOB_DIR
-
 vars="$kob_version $KOB_ARCHIVE_DOWNLOAD_REPO $KOB_NAMESPACE"
 cd ~/KOBman
 git checkout dev
@@ -37,23 +20,18 @@ git checkout $branch
 #copy the tmpl file to /scripts and rename it
 
 cp $KOB_DIR/scripts/tmpl/* $KOB_DIR/scripts/
-echo "a"
 for file in $KOB_DIR/scripts/*.tmpl;
 do
-    echo "b"
     for v in $vars;
     do
-        echo "c"
         sed -i "s/@v@/${v}/g" $file
     done
-    echo "d"
     mv "$file" "${file//.tmpl/}"
-    echo "e"
+    
     
 
 done
 
-exit 0
 
 git add .
 git commit -m "Update version of $branch to $kob_version"
