72c442932a60dba9f47a7262c7df332dad6acfa4
EtricKombat <ericjoy1991@gmail.com>
2020-05-21 16:42:29 +0530

    echo commands fix, kob help modify...new comments for kobman-install.sh commit on: Thu May 21 16:42:29 IST 2020

--- src/main/bash/commands/kobman-install.sh
+++ src/main/bash/commands/kobman-install.sh
@@ -93,13 +93,13 @@ function __kobman_validate_and_set_version
 	__kobman_validate_version_format "${version}" 
 	if [ "$?" -eq "0" ];
 	then
-        	__kobman_check_and_confirm_existing_version "${namespace}" "${version}"
-        	if [ "$?" -eq "0" ];    # check version.txt is empty or not (or version variable is empty or not)
+        	__kobman_check_and_confirm_existing_version "${namespace}" "${version}"  # should introduce case for mapping kobman->KOBman,tob->TheOrgBook,von->von-network 
+        	if [ "$?" -eq "0" ];   							 # check version.txt is empty or not (or version variable is empty or not)
         	then
                 	version_value=$1
         		return 0	
 		else
-                	__kobman_echo_red "version not available"
+                	__kobman_echo_red "Version : ${version} from https://github.com/${namespace_name}/${environment_name} is not available"
 			return 1 
 		fi
 	else
@@ -118,7 +118,9 @@ function __kobman_validate_version_format
 
 function __kobman_check_and_confirm_existing_version
 {
-       	git ls-remote --tags "https://github.com/${1}/KOBman" | grep -w 'refs/tags/[0-9]*\.[0-9]*\.[0-9]*' | sort -r | head | grep -o '[^\/]*$' | grep -w "$2" > /dev/null
+		 
+      	git ls-remote --tags "https://github.com/${1}/KOBman" | grep -w 'refs/tags/[0-9]*\.[0-9]*\.[0-9]*' | sort -r | head | grep -o '[^\/]*$' | grep -w "$2" > /dev/null
+							# version check is happening only for KOBman
 }
 
 function __kobman_create_environment_directory
@@ -128,10 +130,9 @@ function __kobman_create_environment_directory
 		version_id=$2 
         	namespace_name=$3
 		
-		__kobman_echo_green "Directory structure creating for :"  
-               	__kobman_echo_white "Environment 		: ${environment_name} " 
-               	__kobman_echo_white "Version 		: ${version_id}" 
-               	__kobman_echo_white "Namespace		: ${namespace_name}" 
+		__kobman_echo_blue "Installing :"  
+               	__kobman_echo_white "Repo/Namespace	: https://github.com/${namespace_name}/${environment_name}" 
+               	__kobman_echo_white "Version		: ${version_id}" 
 		
 		cd "${KOBMAN_DIR}/envs"
                 mkdir -p kob_env_"${environment_name}"
@@ -149,7 +150,7 @@ function __kobman_create_environment_directory
                 source "${KOBMAN_DIR}/envs/kob_env_${environment_name}/${version_id}/kobman-${environment_name}.sh"
 	#	__kobman_install_"${environment_name}" "${namespace_name}"
 
-		__kobman_echo_blue "Installation of ${environment_name} has been completed !! "	
+		__kobman_echo_blue "Installed from ->  https://github.com/${namespace_name}/${environment_name}"	
 #		__kobman_variable_cleanup
 		cd ~
 }
