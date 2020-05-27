fc3d32ad8dc697906a709078c65a920210964120
EtricKombat <ericjoy1991@gmail.com>
2020-05-27 14:41:21 +0530

    reflecting kob-list with accurate commands commit on: Wed May 27 14:41:21 IST 2020

--- src/main/bash/commands/kobman-list.sh
+++ src/main/bash/commands/kobman-list.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 function __kob_list {
+curl -sL "https://raw.githubusercontent.com/${KOBMAN_NAMESPACE}/KOBman/${KOBMAN_DIST_BRANCH}/dist/list.txt" > tmp.txt
 
-curl -s -L "${KOBMAN_SERVICE}/${KOBMAN_NAMESPACE}/KOBman/${KOBMAN_DIST_BRANCH}/dist/list.txt" > tmp.txt
-
-# envs="Von-Network,TheOrgBook,greenlight,kobman,KOBConnect,KOBRegistry,KochiOrgBook,KOBDflow,KOBVON"
 echo "Available environments and their respective version numbers"
 echo "---------------------------------------------------------------"
-# for i in $envs; do
-#     cat tmp.txt | grep "$i" >> tmp1.txt
-# done
-# cat tmp1.txt
 sed 's/,/ /g' tmp.txt > tmp1.txt
 sed 's/,/ /g' tmp.txt | cut -d " " -f 1 > tmp2.txt
 cat tmp1.txt | grep -h -f tmp2.txt > tmp3.txt
