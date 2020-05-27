423ba5ecb45961ebbded36fe024fd0936c1e57f8
EtricKombat <ericjoy1991@gmail.com>
2020-05-27 14:26:27 +0530

    list command testing commit on: Wed May 27 14:26:27 IST 2020

--- src/main/bash/commands/kobman-list.sh
+++ src/main/bash/commands/kobman-list.sh
@@ -2,6 +2,18 @@
 
 function __kob_list {
 
-curl -L "${KOBMAN_SERVICE}/${KOBMAN_NAMESPACE}/KOBman/master/dist/list" | less
+curl -s -L "${KOBMAN_SERVICE}/${KOBMAN_NAMESPACE}/KOBman/${KOBMAN_DIST_BRANCH}/dist/list.txt" > tmp.txt
 
+# envs="Von-Network,TheOrgBook,greenlight,kobman,KOBConnect,KOBRegistry,KochiOrgBook,KOBDflow,KOBVON"
+echo "Available environments and their respective version numbers"
+echo "---------------------------------------------------------------"
+# for i in $envs; do
+#     cat tmp.txt | grep "$i" >> tmp1.txt
+# done
+# cat tmp1.txt
+sed 's/,/ /g' tmp.txt > tmp1.txt
+sed 's/,/ /g' tmp.txt | cut -d " " -f 1 > tmp2.txt
+cat tmp1.txt | grep -h -f tmp2.txt > tmp3.txt
+cat tmp3.txt
+rm tmp*
 }
--- src/main/bash/commands/kobman-status.sh
+++ src/main/bash/commands/kobman-status.sh
@@ -3,10 +3,27 @@
 
 function __kob_status {
 
-sudo rm ${KOBMAN_CANDIDATES_DIR}/*.tar.gz 2> /dev/null
-sudo rm ${KOBMAN_CANDIDATES_DIR}/source-to-image-v1.1.14-874754de-linux-amd64.tar.gz.* 2> /dev/null
-__kobman_echo_cyan "=========STATUS========="
-cd ${KOBMAN_CANDIDATES_DIR} 
-sudo ls -d Dev_* 2> /dev/null
+file=($(find $KOBMAN_DIR/envs/ -name "kob_env_*" -print))
+__kobman_echo_yellow "Installed environments/ version"
+__kobman_echo_yellow "--------------------------------"
+for f in "${file[@]}"; do
+	# echo "$f"
+    n=${f##*_}
+    # cd $KOBMAN_DIR/envs/kob_env_$n
+    if [[ $n == $(cat $KOBMAN_DIR/var/current) ]]; then
+        echo "~" $n $(ls $KOBMAN_DIR/envs/kob_env_$n | grep -v $(cat $KOBMAN_DIR/var/current)) $(cat $KOBMAN_DIR/var/current)"*" > $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
+        sed 's/current//g' $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
+    else
 
+        echo $n $(ls $KOBMAN_DIR/envs/kob_env_$n | grep -v $(cat $KOBMAN_DIR/var/current)) $(cat $KOBMAN_DIR/var/current)"*" > $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
+        sed 's/current//g' $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
+    fi
+    rm $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
+    
+done
+# echo $file
+
+# cat tmp.txt
+
+unset n file f 
 }
