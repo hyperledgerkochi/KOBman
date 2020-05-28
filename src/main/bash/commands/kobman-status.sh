c4a64690ce031c5ccd4e0cf207263c99518c2119
Arun Suresh <arunsureshampadath@gmail.com>
2020-05-28 20:15:20 +0530

    Updated

--- src/main/bash/commands/kobman-status.sh
+++ src/main/bash/commands/kobman-status.sh
@@ -2,25 +2,24 @@
 
 
 function __kob_status {
-
-cd $KOBMAN_DIR/envs
-file=($(find -name "kob_env_*" -print))
+file=($(find $KOBMAN_DIR/envs/ -name "kob_env_*" -print))
 echo "Installed environments and their version"
 echo "---------------------------------------------"
 for f in "${file[@]}"; do
-	# echo "$f"
+	
     n=${f##*_}
-    cd $KOBMAN_DIR/envs/kob_env_$n
+    if [[ $n == $(cat $KOBMAN_DIR/var/current) ]]; then
+        echo "~" $n $(ls $KOBMAN_DIR/envs/kob_env_$n | grep -v $(cat $KOBMAN_DIR/envs/kob_env_$n/current)) $(cat $KOBMAN_DIR/envs/kob_env_$n/current)"*" > $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
+        sed 's/current//g' $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
+    else
 
-
-    echo $n $(ls $KOBMAN_DIR/envs/kob_env_$n | grep -v $(cat current)) $(cat current)"*" > tmp.txt
-    sed 's/current//g' $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
-    rm tmp.txt
+        echo $n $(ls $KOBMAN_DIR/envs/kob_env_$n | grep -v $(cat $KOBMAN_DIR/envs/kob_env_$n/current)) $(cat $KOBMAN_DIR/envs/kob_env_$n/current)"*" > $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
+        sed 's/current//g' $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
+    fi
+    rm $KOBMAN_DIR/envs/kob_env_$n/tmp.txt
     
 done
-# echo $file
-
-# cat tmp.txt
 
 unset n file f 
+
 }
