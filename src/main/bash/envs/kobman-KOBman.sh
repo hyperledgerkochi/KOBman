e8d05a35de3a5232dde81f0c67d301daebbbee41
EtricKombat <ericjoy1991@gmail.com>
2020-06-09 15:30:52 +0530

    removing HEAD>>>> message from KOBman.sh commit on: Tue Jun  9 15:30:52 IST 2020

--- src/main/bash/envs/kobman-KOBman.sh
+++ src/main/bash/envs/kobman-KOBman.sh
@@ -28,18 +28,8 @@ function __kobman_install_KOBman
 
 }
 
-function __kobman_create_dev_environment
-=======
-
-
-
-#################newly incorporated code from kobma-dependancy.sh################
-
-
-
 
 function __kobman_create_dev_environment 
->>>>>>> 0ca31736b3bf96abd68fdf1642d9dc2c1b32f220
 {
 	local environment="$1"
 
@@ -49,71 +39,4 @@ function __kobman_create_dev_environment
 	export Dev_${environment}=$PWD
 	mkdir -p test/ dependency/
 }
-<<<<<<< HEAD
-=======
 	
-
-
-
-################################old codes to refer if required ################
-
-
-
-
->>>>>>> 0ca31736b3bf96abd68fdf1642d9dc2c1b32f220
-
-
-
-#function __kobman_uninstall_kobman
-#{
-# 	__kobman_echo_white "Removing KOBman environment..."
-<<<<<<< HEAD
-#	cd ${KOBMAN_KOBMAN_DEV_DIR}
-#	rm -rf KOBman/ 2> /dev/null
-#	cd ~
-#	rm -rf Dev_KOBman/ 2> /dev/null
-#	rm -rf ${KOBMAN_KOBMAN_DEV_DIR} 2> /dev/null
-#        cd ~
-#	__kobman_echo_red "KOBman environment removed !!"
-=======
-#	cd ${KOBMAN_KOBMAN_DEV_DIR} 
-#	rm -rf KOBman/ 2> /dev/null	
-#	cd ~
-#	rm -rf Dev_KOBman/ 2> /dev/null	
-#	rm -rf ${KOBMAN_KOBMAN_DEV_DIR} 2> /dev/null	
-#        cd ~
-#	__kobman_echo_red "KOBman environment removed !!"	
->>>>>>> 0ca31736b3bf96abd68fdf1642d9dc2c1b32f220
-#}
-#
-#function __kobman_version_kobman
-#{
-<<<<<<< HEAD
-#	if [ -d "${KOBMAN_KOBMAN_DEV_DIR}" ]; then
-##		namespace="$1"
-##		cd ${KOBMAN_KOBMAN_DEV_DIR}
-##		cd KOBman/
-=======
-#	if [ -d "${KOBMAN_KOBMAN_DEV_DIR}" ]; then 
-##		namespace="$1"
-##		cd ${KOBMAN_KOBMAN_DEV_DIR} 
-##		cd KOBman/	
->>>>>>> 0ca31736b3bf96abd68fdf1642d9dc2c1b32f220
-##		git show-ref --tag | grep -o v0.0.*
-# #		cd "${KOBMAN_DIR}"/var
-#		cd "${KOBMAN_DIR}/var"
-#                cat version.txt
-#                cd ~
-#	else
-<<<<<<< HEAD
-# 		__kobman_echo_red "KOBman Environment is not installed in the Local system !"
-#fi
-#
-#}
-=======
-# 		__kobman_echo_red "KOBman Environment is not installed in the Local system !"	
-#fi
-#
-#}
-#
->>>>>>> 0ca31736b3bf96abd68fdf1642d9dc2c1b32f220
