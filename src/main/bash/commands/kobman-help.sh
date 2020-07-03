0fcf53be45d59175a180ffac6b342d8db09ff472
ananashraf <ananashraf2@gmail.com>
2020-07-03 16:37:23 +0530

    updated

--- src/main/bash/commands/kobman-help.sh
+++ src/main/bash/commands/kobman-help.sh
@@ -9,7 +9,7 @@ function __kob_help {
 	__kobman_echo_white "uninstall     : $ kob uninstall --environment [env_name]\n\t\tor\n\t\t$ kob uninstall –env [env_name] -V [version]\n\t\tThe command is used to uninstall the specified\n\t\tenvironment.\n\t\tEg: $ kob uninstall –env KOBman\n"
 	__kobman_echo_white "status        : $ kob status\n\t\tThe command displays the installed environments.\n"
 	__kobman_echo_white "list          : $ kob list\n\t\tThe command lists the various environment that\n\t\tcan be installed.\n"
-	__kobman_echo_white "selfupdate    : $ kob selfupdate\n\t\tUpdates KOBman to next version available.\n"
+	__kobman_echo_white "upgrade       : $ kob upgrade\n\t\tUpgrades KOBman to next version available.\n"
 	__kobman_echo_white "update        : $ kob update\n\t\tUpdates the list file with lastest changes.\n"
 	__kobman_echo_white "version       : Version of KOBman Utility\n\t\t-------------------------\n\t\t$ kob --version or kob –V\n\t\tThis command displays the version of KOBman\n\t\tinstalled on the host."
 	__kobman_echo_white "\n\t\tVersion of an Environment\n\t\t-------------------------\n\t\t$ kob -V -env [env_name]\n\t\tThe command displays the version of the specified\n\t\tenvironment.\n\t\tEg: $ kob -V -env greenlight\n"
